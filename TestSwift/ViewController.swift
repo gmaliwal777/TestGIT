88df1fd8bb0a55ae2f1c405a39870fac9fad4885
Ghanshyam <gmaliwal777@gmail.com>
2018-07-21 17:31:08 +0530

    Initial Commit

---
+++ TestSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestSwift
+//
+//  Created by Macmini on 7/21/18.
+//  Copyright © 2018 Macmini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
