90b315927a61a93b49c56f8303394efa5ac22b13
Ghanshyam <gmaliwal777@gmail.com>
2018-08-19 16:40:28 +0530

    first commit

--- TestSwift/ViewController.swift
+++ TestSwift/ViewController.swift
@@ -8,18 +8,53 @@
 
 import UIKit
 
+
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scrollView : UIScrollView!
+    @IBOutlet weak var userNameTextField : UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(handleKeyboardShowEvent(notification:)), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(handleKeyboardHideEvent(notification:)), name: NSNotification.Name.UIKeyboardDidShow, object: nil)
+        
     }
 
+    func handleKeyboardShowEvent( notification : Notification) {
+        print("notification ---- \(notification)")
+        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+            let contentInset = UIEdgeInsets(top: 0.0, left: 0.0, bottom: keyboardSize.height+300, right: 0.0)
+            scrollView.contentInset = contentInset
+        }
+        
+    }
+    
+    
+    func handleKeyboardHideEvent( notification : Notification) {
+        print("notification ---- \(notification)")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        //print("size of device --- \()")
+    }
+    
+    @IBAction func login() {
+        print("userNameTextField frame ---- \(userNameTextField.frame)")
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    
+    
 }
 
